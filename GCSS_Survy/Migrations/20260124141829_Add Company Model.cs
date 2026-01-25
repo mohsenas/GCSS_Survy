ce8350592817523f025b5971fb750deb2a49489c
mohsenas <mohsenas020@gmail.com>
2026-01-25 20:29:17 +0300

    migrations

---
+++ GCSS_Survy/Migrations/20260124141829_Add Company Model.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GCSS_Survy.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCompanyModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Emp_Simples",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.CreateTable(
+                name: "Companies",
+                columns: table => new
+                {
+                    ID = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CompanyName = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: false),
+                    Address = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ManagerId = table.Column<int>(type: "int", nullable: false),
+                    IsEnable = table.Column<bool>(type: "bit", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false),
+                    AddTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    LastModifyTime = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    UserID = table.Column<int>(type: "int", nullable: false),
+                    BranchID = table.Column<int>(type: "int", nullable: false),
+                    SessionID = table.Column<long>(type: "bigint", nullable: false),
+                    Notes = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: true),
+                    RV = table.Column<byte[]>(type: "varbinary(max)", nullable: true),
+                    LastModifiedUserId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Companies", x => x.ID);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Companies_CompanyName",
+                table: "Companies",
+                column: "CompanyName",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Companies");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Emp_Simples");
+        }
+    }
+}
